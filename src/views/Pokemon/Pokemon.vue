4fa82822b05090d41aaca53b83f0d5a85d7ad174
Ullisses Santos Neves Azevedo <ullisses.neves@gmail.com>
2021-08-20 14:32:28 -0300

    added AboutTabComponent, improve lazy loading and any fix

--- src/views/Pokemon/Pokemon.vue
+++ src/views/Pokemon/Pokemon.vue
@@ -2,23 +2,22 @@
   <div>
     <v-card v-if="!loading" class="mx-auto">
       <v-sheet :color="bgElementColors.get(pokemonType)">
-        <v-row no-gutters align="center">
-          <router-link to="/pokemon">
+        <v-row no-gutters >
+          <v-col cols="6">
+            <router-link to="/pokemon">
             <v-btn depressed icon class="float-start">
               <v-icon>mdi-arrow-left</v-icon>
             </v-btn>
           </router-link>
-          <v-col cclass="text-center">
             <v-list-item three-line>
               <v-list-item-content>
                 <v-list-item-subtitle
-                  class="pt-12 pb-0 font-weight-bold pl-5 text-h6"
+                  class="pt-12 pb-0 font-weight-bold text-h6"
                 >
                   <span color="white" class="text--secondary" >{{ pokemonId }}</span>
                 </v-list-item-subtitle>
                 <v-list-item-title
                   class="
-                    pl-5
                     pb-0
                     text-capitalize
                     white--text
@@ -28,11 +27,11 @@
                 >
                   {{ pokemon.name }}
                 </v-list-item-title>
-                <v-list-item-subtitle class="pl-5">
+                <v-list-item-subtitle>
                   <v-chip-group>
                     <v-chip
                       label
-                      class="text-capitalize px-2 font-weight-bold"
+                      class="text-capitalize font-weight-bold"
                       text-color="white"
                       v-for="(type, i) in pokemon.types"
                       :key="i"
@@ -40,6 +39,7 @@
                     >
                       <v-avatar dark class="mr-1">
                         <v-img 
+                          contain
                           :style="chipSvgMas(type.type.name)"
                         ></v-img>
                       </v-avatar>
@@ -53,10 +53,10 @@
 
           <v-col cols="6">
             <v-img
-              max-height="245"
+              max-height="100%"
               :src="pokemonSprite"
               :alt="pokemon.name"
-              class="pr-12 ma-10"
+              class="ma-10"
               contain
             >
             </v-img>
@@ -79,31 +79,33 @@
             <component v-if="!loading" :pokemon="pokemon" :is="tab.component"></component>
         </v-tab-item>
       </v-tabs-items>
-
-      
-      <!-- <pre>{{ pokemon }}</pre> -->
+      <!-- <pre>{{ pokemon.locations }}</pre> -->
     </v-card>
   </div>
 </template>
 
 <script>
 import Colors from "../../core/colors";
-import StatusTab from './PokemonTabs/StatsTab.vue'
+const StatusTab = () => import('./PokemonTabs/StatsTab.vue');
+const AboutTab = () => import('./PokemonTabs/AboutTab.vue');
 
 export default {
   name: "pokemon",
   components: {
-    StatusTab
+    StatusTab,
+    AboutTab
   },
   data: function () {
     return {
       tab: null,
       tabs:[
+        {label:'About', component:'about-tab'},
         {label:'Stats', component:'status-tab'},
-        {label:'About', component:''},
-        {label:'Location', component:''}
+        {label:'Evolutions', component:''}
       ],
       loading: false,
+      loadingSpecies: false,
+      loadingLocations: false,
       pokemon: null,
     };
   },
@@ -148,13 +150,24 @@ export default {
           this.$api.get(`${this.pokemon.location_area_encounters}`)
           .then((location) => {
             this.pokemon.locations = location.data
-            this.loading = false;
+
+            this.$api.get(`${this.pokemon.species.url}`)
+            .then((specie) => {
+              this.pokemon.species.data = (({evolution_chain,flavor_text_entries, genera})=>({evolution_chain,flavor_text_entries, genera}))(specie.data)
+              
+              this.$api.get(`${this.pokemon.species.data.evolution_chain.url}`)
+              .then((evolutions) => {
+                this.pokemon.species.data.evolution_chain.data = evolutions.data
+                  this.loading = false;
+              })
+            })
           })
         })
         .catch((e) => {
           this.errors.push(e);
           this.loading = false;
         });
+        
     },
   },
   beforeMount() {
---
+++ src/views/Pokemon/PokemonTabs/AboutTab.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-card-text>
+    <v-list dense>
+      <v-list-item-title
+        class="text-h6"
+        :style="{ color: colors.get(pokemonType) }"
+      >
+        <v-icon :color="colors.get(pokemonType)">mdi-pokeball</v-icon>
+        Pokemon Data
+      </v-list-item-title>
+      <template>
+        <v-list-item class="text-capitalize">
+          <v-container>
+            <v-row>
+              <v-col md="2">
+                Species:
+              </v-col>
+              <v-col md="auto">
+                {{ pokemonSpecie }}
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col md="2">
+                Height:
+              </v-col>
+              <v-col md="auto">
+                {{ pokemon.height }}
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col md="2">
+                Weight:
+              </v-col>
+              <v-col md="auto">
+                {{ pokemon.weight }}
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col md="2">
+                Abilities:
+              </v-col>
+              <v-col>
+                <v-list-item
+                  v-for="abilities in pokemon.abilities"
+                  :key="abilities.ability.name"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      {{ abilities.ability.name }}
+                    </v-list-item-title>
+                    <small v-if="abilities.is_hidden">(hidden ability)</small>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-list-item>
+      </template>
+    </v-list>
+
+    <v-list dense>
+      <v-list-item-title
+        class="text-h6"
+        :style="{ color: colors.get(pokemonType) }"
+      >
+        <v-icon :color="colors.get(pokemonType)">mdi-pokemon-go</v-icon>
+        Location
+      </v-list-item-title>
+      <template>
+        <v-list-item
+          two-line
+          v-for="(location, i) in pokemon.locations"
+          :key="i"
+          class="text-capitalize"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ shortedNameArea(location.location_area.name) }}
+            </v-list-item-title>
+            <v-list-item-subtitle
+              >{{ getPokemonVersions(location) }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+    <pre>{{
+      pokemon.species.data.evolution_chain.data.chain.species.name
+    }}</pre>
+    <pre>{{
+      pokemon.species.data.evolution_chain.data.chain.evolves_to[0].species.name
+    }}</pre>
+    <pre>{{
+      pokemon.species.data.evolution_chain.data.chain.evolves_to[0]
+        .evolves_to[0].species.name
+    }}</pre>
+  </v-card-text>
+</template>
+<script>
+import Colors from "../../../core/colors";
+
+export default {
+  name: "about-tab",
+  props: ["pokemon"],
+  data: function() {
+    return {};
+  },
+  computed: {
+    pokemonSpecie() {
+      const specie = this.pokemon.species.data.genera.filter(
+        (t) => t.language.name == "en"
+      );
+      return specie[0].genus;
+    },
+    colors() {
+      return Colors.backgroundTypeColors;
+    },
+    pokemonType() {
+      return this.pokemon.types[0].type.name;
+    },
+  },
+  methods: {
+    getPokemonVersions(pokemon) {
+      const versions = pokemon.version_details.map(
+        (element) => element.version.name
+      );
+      return versions.join(", ");
+    },
+    shortedNameArea(areaName) {
+      if (areaName) {
+        let shortedName = areaName.split("-");
+        return shortedName.join(" ").replace("area", "");
+      } else return "Location Not Found";
+    },
+  },
+};
+</script>
+<style></style>
